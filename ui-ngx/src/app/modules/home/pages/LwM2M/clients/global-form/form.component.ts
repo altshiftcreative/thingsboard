e5dd55d08330c1c8802cfccb953abe810e2ed85f
obada <obadatumah19@gmail.com>
2021-01-19 12:34:51 +0200

    get client fixed

--- ui-ngx/src/app/modules/home/pages/LwM2M/clients/global-form/form.component.ts
+++ ui-ngx/src/app/modules/home/pages/LwM2M/clients/global-form/form.component.ts
@@ -20,6 +20,7 @@ export class formDialog implements OnInit {
     formGroupObject = {};
     dataType = this.lwService.formType;
     ngOnInit() {
+
         for (let item of this.resourcesArray) {
             if (item['mandatory']) this.formGroupObject[item['name']] = new FormControl(null, Validators.required);
             else this.formGroupObject[item['name']] = new FormControl(null);
@@ -29,36 +30,72 @@ export class formDialog implements OnInit {
 
 
     async onSubmit() {
-        let dataNames = [];
-        this.resourcesArray.forEach((item, index) => {
-            if (this.newInstanceForm.value[item['name']] != null) {
-                if (isNaN(this.newInstanceForm.value[item['name']]))
-                    dataNames.push({ "id": item['id'], "value": this.newInstanceForm.value[item['name']] })
-                else
-                    dataNames.push({ "id": item['id'], "value": parseInt(this.newInstanceForm.value[item['name']]) })
+        if (this.lwService.formType['type'] != 'write') {
 
-            }
-        })
 
-        if (this.lwService.formType['type'] == 'update') {
 
-            await this.http.put(this.lwService.lwm2mBaseUri + '/api/v1/Lw/update/?endpoint=' + this.lwService.clientEndpoint + '&value=' + this.lwService.value + '&format=' + this.lwService.format + '&timeout=' + this.lwService.timeout + '&replace=' + 'false',
-                {
-                    "id": this.lwService.value[1],
-                    "resources": dataNames
+
+            let dataNames = [];
+            this.resourcesArray.forEach((item, index) => {
+                if (this.newInstanceForm.value[item['name']] != null) {
+                    if (isNaN(this.newInstanceForm.value[item['name']]))
+                        dataNames.push({ "id": item['id'], "value": this.newInstanceForm.value[item['name']] })
+                    else
+                        dataNames.push({ "id": item['id'], "value": parseInt(this.newInstanceForm.value[item['name']]) })
+
                 }
-            ).toPromise().then((dta) => { })
-            this.lwService.progress("CHANGED", true);
+            })
+
+            if (this.lwService.formType['type'] == 'update') {
+
+                await this.http.put(this.lwService.lwm2mBaseUri + '/api/v1/Lw/update/?endpoint=' + this.lwService.clientEndpoint + '&value=' + this.lwService.value + '&format=' + this.lwService.format + '&timeout=' + this.lwService.timeout + '&replace=' + 'false',
+                    {
+                        "id": this.lwService.value[1],
+                        "resources": dataNames
+                    }
+                ).toPromise().then((dta) => { })
+                this.lwService.progress("CHANGED", true);
+            }
+
+            else if (this.lwService.formType['type'] == 'create') {
+                await this.http.post(this.lwService.lwm2mBaseUri + '/api/v1/Lw/instance/?endpoint=' + this.lwService.clientEndpoint + '&value=' + this.lwService.value + '&format=' + this.lwService.format + '&timeout=' + this.lwService.timeout,
+
+                    {
+                        "resources": dataNames
+                    }
+                ).toPromise().then((dta) => { })
+                this.lwService.progress("CREATED", true);
+            }
         }
 
-        else if (this.lwService.formType['type'] == 'create') {
-            await this.http.post(this.lwService.lwm2mBaseUri + '/api/v1/Lw/instance/?endpoint=' + this.lwService.clientEndpoint + '&value=' + this.lwService.value + '&format=' + this.lwService.format + '&timeout=' + this.lwService.timeout,
+        else if (this.lwService.formType['type'] == 'write') {
+            let enterdValue;
+            
+            if (this.newInstanceForm.value[this.lwService.formData[0]['name']] != null) {
+                if (isNaN(this.newInstanceForm.value[this.lwService.formData[0]['name']])) {
+                     enterdValue = this.newInstanceForm.value[this.lwService.formData[0]['name']]
+                }
+                else {
+                     enterdValue = parseInt(this.newInstanceForm.value[this.lwService.formData[0]['name']]);
+                }
+            }
+            
+            await this.http.put(this.lwService.lwm2mBaseUri + '/api/v1/Lw/write/?endpoint=' + this.lwService.clientEndpoint + '&value=' + this.lwService.value + '&format=' + this.lwService.format + '&timeout=' + this.lwService.timeout,
 
                 {
-                    "resources": dataNames
+                    "id": this.lwService.formType['id'],
+                    "value": enterdValue
                 }
-            ).toPromise().then((dta) => { })
-            this.lwService.progress("CREATED", true);
+            ).toPromise().then((writeData) => {
+                if (writeData['failure']) {
+                    this.lwService.progress(writeData['status'], false);
+                }
+                else {
+                    this.lwService.finalWriteValue = enterdValue
+                    this.lwService.progress("SUCCESS", true);
+                }
+            })
+            
         }
     }
 }
